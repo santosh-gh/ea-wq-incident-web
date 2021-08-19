65989f17e2f0f556a2b8395383507348d52fa762
David Stone <davidjamesstone@gmail.com>
2021-08-19 11:04:29 +0100

    Remove mineType reference

--- app/routes/incident/question-page-options.js
+++ app/routes/incident/question-page-options.js
@@ -1,5 +1,4 @@
 const sessionHandler = require('../../services/session-handler')
-const { asArray } = require('../../util/array-util')
 
 const questionPageOptions = {
   ext: {
@@ -7,9 +6,6 @@ const questionPageOptions = {
       method: async (request, h) => {
         if (request.app['hapi-govuk-question-page']) {
           const dataToSet = request.app['hapi-govuk-question-page'].data
-          if (dataToSet.mineType !== undefined) {
-            dataToSet.mineType = asArray(dataToSet.mineType)
-          }
           sessionHandler.update(request, 'complaint', dataToSet)
         }
         return h.continue
