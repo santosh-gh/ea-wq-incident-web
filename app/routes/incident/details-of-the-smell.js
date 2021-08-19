0e091290af99cb55d703fb53f985c1d2e970799a
David Stone <davidjamesstone@gmail.com>
2021-08-19 13:00:07 +0100

    Fix error

--- app/routes/incident/details-of-the-smell.js
+++ app/routes/incident/details-of-the-smell.js
@@ -13,23 +13,24 @@ module.exports = [
         },
         getData: (request) => {
           const data = sessionHandler.get(request, 'complaint')
-          const { smellDescription, dateOfSmell, timeOfSmell } = data
-          const date = dateOfSmell ? new Date(dateOfSmell) : undefined
-          return { smellDescription, dateOfSmell: date, timeOfSmell }
-        },
-        setData: async (request, data) => {
-          const dateOfSmell = data.dateOfSmell
-          if (dateOfSmell > new Date()) {
-            return {
-              errors: {
-                titleText: 'Fix the following errors',
-                errorList: [
-                  { href: '#dateOfSmell', name: 'dateOfSmell', text: 'Date has to be in the past' }
-                ]
-              }
-            }
+          if (data.dateOfSmell) {
+            data.dateOfSmell = new Date(data.dateOfSmell)
           }
+          return data
         },
+        // setData: async (request, data) => {
+        //   const dateOfSmell = data.dateOfSmell
+        //   if (dateOfSmell > new Date()) {
+        //     return {
+        //       errors: {
+        //         titleText: 'Fix the following errors',
+        //         errorList: [
+        //           { href: '#dateOfSmell', name: 'dateOfSmell', text: 'Date has to be in the past' }
+        //         ]
+        //       }
+        //     }
+        //   }
+        // },
         getNextPath: () => './confirmation',
         pageDefinition: require('./page-definitions/details-of-the-smell')
       }
